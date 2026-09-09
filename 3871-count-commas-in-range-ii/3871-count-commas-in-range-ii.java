ecf3a342a9c25ad395f3ea55e91bcaeafe0fecb4
Sarvesh-88 <sk70689153@gmail.com>
2026-09-09 22:00:31 +0530

    Time: 1 ms (99.36%), Space: 42.4 MB (93.59%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public long countCommas(long n) {
-        long totalCommas = 0;
+      long totalCommas = 0;
         
         // Start at the first comma boundary (1,000) and multiply by 1,000 each step
         for (long limit = 1000; limit <= n; limit *= 1000) {
